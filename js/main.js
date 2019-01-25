4aa74854d71a437f51adf2ef823da1af6e03d48a
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-01-25 10:36:03 -0500

    feat: JQuery vs VanillaJS

--- js/main.js
+++ js/main.js
@@ -1,13 +1,31 @@
 const compose = (...functions) => data =>
   functions.reduceRight((value, func) => func(value), data)
 
+/* JQuery */
 // let description = $('#description')
 // let calories = $('#calories')
 // let carbs = $('#carbs')
 // let protein = $('#protein')
 
-// Get the value --> description.val()
+// // Get the value --> description.val()
 
+// const validateInputs = () => {
+//   description.value ? '' : description.addClass('is-invalid')
+//   calories.value ? '' : calories.addClass('is-invalid')
+//   carbs.value ? '' : carbs.addClass('is-invalid')
+//   protein.value ? '' : protein.addClass('is-invalid')
+
+//   if(description.val() && calories.val() && carbs.val() && protein.val()) {
+//     console.log('OK!')
+//   }
+// }
+
+// description.keypress(() => description.removeClass('is-invalid'))
+// calories.keypress(() => calories.removeClass('is-invalid'))
+// carbs.keypress(() => carbs.removeClass('is-invalid'))
+// protein.keypress(() => protein.removeClass('is-invalid'))
+
+/* VanillaJS */
 let description = document.getElementById('description')
 let calories = document.getElementById('calories')
 let carbs = document.getElementById('carbs')
