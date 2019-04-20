caba79882abfa2459423789e674490b582f360eb
Nathaly Riaño Bejarano <natharb09@gmail.com>
2019-04-19 21:05:42 -0500

    feat: Completing the functions to generate HTML tags

--- js/main.js
+++ js/main.js
@@ -35,6 +35,30 @@ const inputs = [description, calories, carbs, protein]
 
 let list = []
 
+const attrsToString = (obj = {}) => {
+  const keys = Object.keys(obj)
+  const attrs = []
+
+  for (let i = 0; i < keys.length; i++) {
+    let attr = keys[i]
+    attrs.push(`${attr}="${obj[attr]}"`)
+  }
+
+  const string = attrs.join('')
+
+  return string
+}
+
+const tagAttrs = obj => (content = '') => `<${obj.tag} ${obj.attrs ? ' ' : ''}${attrsToString(obj.attrs)}>${content}</${obj.tag}>`
+
+const tag = t => {
+  if(typeof t === 'string') {
+    tagAttrs({tag: t})
+  } else {
+    tagAttrs(t)
+  }
+}
+
 const showInvalid = input => input.value ? '' : input.classList.add('is-invalid')
 
 const isValid = input => input.value ? true : false 
